f8988a90d46cb9c8b806ef0823a0415831e34191
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2017-08-12 12:18:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var counter = 0;
+app.get('/ui/style.css', function (req, res) {
+    counter = counter +1;
+  res.send(path.join(counter.toString()));
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
