e8da9aeb4431a53e08434293b4ad5660e2867f70
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2017-08-12 12:45:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,10 @@ img.onclick = function (){
     var interval = setInterval(moveRight, 50);
     img.style.marginLeft = '100px';
 };
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function (){
+  counter = counter + 1;
+  var span = document.getElementById('count')
+  span.innerHTML = counter.toString();
+};
