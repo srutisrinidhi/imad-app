60af20ef882f2ca65c49233dbb747c38b7a35858
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2017-08-12 13:10:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,17 @@
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function() {
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = funtion () {
+      if (request.readyState === XMLHttpREquest.DONE) {
+          if (request.status ===200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+          }
+          
+      }
+  }
+  request.open('GET', 'http://srutisrinidhi.imad.hasura-app.io', true);
+  request.send(null);
+  
 };
