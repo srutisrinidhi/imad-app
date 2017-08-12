9307d75bb85c9a8abab1acb9e8b644483d019a49
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2017-08-12 13:45:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,16 @@ button.onclick = function() {
   request.send(null);
   
 };
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onClick = function() {
+    var names = ['name1', 'name2', 'name3'];
+    var list = "";
+    for (var i=0; i= names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHtml = list;
+};
