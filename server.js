cba976e9977bb39d2f42be68ba20d2c69d1ffc7c
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2017-08-28 21:05:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article/:articleName', function(req,res){
-    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result){
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result){
        if (err){
            res.status(500).send(err.toString());
             } 
