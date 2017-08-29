4c1d896ab8e7ecfd5a2b2ba5d4ec56f9fc9f488d
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2017-08-29 18:48:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,14 @@ app.get('/test-db', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+function hash(input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512 , 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function (req, res) {
+  var hashedString = hash(req.params.input, 'this-is-a-random-string');
+  res.send(hashedString);
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
